2f6863f08df92cc9453332c4dcb00ff92ab20904
only1drhex <lordrhexx@gmail.com>
2022-04-19 19:59:34 +0000

    testing 2

--- examples/example1.js
+++ examples/example1.js
@@ -1,16 +1,12 @@
 const ytsearch = require("../")
 
 const main = async() =>{
-<<<<<<< HEAD
   let saveString;
   let results = await ytsr("Black Panther")
-=======
   let results = await ytsearch("Black Panther")
   
   console.log(results)
->>>>>>> 1dffa428c217be2c1fb4ae15825de634b45f532f
-  
-  
+
   
 }
 main()
